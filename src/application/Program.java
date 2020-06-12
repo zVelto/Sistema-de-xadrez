d6ab0a6a6a208c24ebcb079c15640a7a20726e9b
Welton Carvalho <weltoncarvalhoo@gmail.com>
2020-06-11 21:03:40 -0300

    Primeira classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Position pos = new Position(3, 5);
+        System.out.println(pos);
     }
     
 }
